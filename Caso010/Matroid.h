624f6491593d9b6eed27235c80c743a71a550c2f
María José Cortés <maria2000jose.mj@gmail.com>
2019-10-28 23:28:13 -0600

    feat-cascarón
    clases que se van a necesitar

---
+++ Caso010/Matroid.h
@@ -0,0 +1,14 @@
+//
+// Created by María José on 10/28/2019.
+//
+
+#ifndef CASO010_MATROID_H
+#define CASO010_MATROID_H
+
+
+class Matroid{
+
+};
+
+
+#endif //CASO010_MATROID_H
---
+++ Caso010/matroidEvaluator.h
@@ -0,0 +1,14 @@
+//
+// Created by María José on 10/28/2019.
+//
+
+#ifndef CASO010_MATROIDEVALUATOR_H
+#define CASO010_MATROIDEVALUATOR_H
+
+
+class matroidEvaluator {
+
+};
+
+
+#endif //CASO010_MATROIDEVALUATOR_H
