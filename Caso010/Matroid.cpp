9390a45600e48066f4ae5423c7a081c557fcd57e
María José Cortés <maria2000jose.mj@gmail.com>
2019-11-07 01:43:31 -0600

    feat-parte b
    parallel calculation of matroids intersection

--- Caso010/Matroid.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by María José on 10/28/2019.
-//
-
-#include "Matroid.h"
-//Empty
--- Caso010/Matroid.h
+++ Caso010/Matroid.h
@@ -13,14 +13,15 @@ struct Matroid
     void *S[M_SIZE];   //Pointer to S in the matroid.
     void *I[M_SIZE];   //Pointer to I in the matroid.
     void (*func)(struct Matroid*);    //Pointer to the function.
+    int Isize;
 };
 
-// Function that uses the function given to evaluate de matroid //
+// Function that uses the function given to evaluate the matroid //
 void processingM(struct Matroid matrArray[],int arrSize){
-#pragma omp parallel for
+#pragma omp parallel for // NOLINT(openmp-use-default-none)
     for(int elemntInArray = 0; elemntInArray < arrSize; elemntInArray++ ){
         int id = omp_get_thread_num();
-        printf("\n Matroid %d is tested in thread %d \n",elemntInArray+1,id);
+      //  printf("\n Matroid %d is tested in thread %d \n",elemntInArray+1,id);
         matrArray[elemntInArray].func(&matrArray[elemntInArray]);
     }
 }
@@ -42,6 +43,49 @@ void printChars(void *array[], int arrSize){
     }
     printf("\n");
 }
+//busca un elemento en un arreglo (en este caso en el subset I)
+bool elementExists(void *array[],int arrSize, int pElement){
+    for(int arrayPos=0; arrayPos<arrSize; arrayPos++){
+        if((int) array[arrayPos] == pElement){
+            return true;
+        }
+    }
+    return false;
+}
+
+//searchs the current item in the matroid array (Paralelismo aquí)
+bool isIntersection(struct Matroid matrArray[],int arrSize,int pElement){
+    bool result = true;
+#pragma omp parallel for // NOLINT(openmp-use-default-none)
+    for(int matrArrayPos=0; matrArrayPos<arrSize;matrArrayPos++){
+
+       // int id = omp_get_thread_num();
+       // printf("\n Matroid %d is tested in thread %d \n",matrArrayPos+1,id);
+
+        int size =  matrArray[matrArrayPos].Isize;
+        if(!elementExists(matrArray[matrArrayPos].I,size,pElement)){
+            result = false;
+        }
+    }
+    return result;
+}
+
+void CalculateIntersection(struct Matroid matrArray[],int arrSize){
+    int size =  matrArray[0].Isize;
+    printf("Intersection is:\n");
+    for(int matrArrayI=0; matrArrayI<size;matrArrayI++){
+        int actualNumber = (int) matrArray[0].I[matrArrayI];
+        if(isIntersection(matrArray, arrSize, actualNumber)){
+            printf("%i \n", actualNumber);
+        }
+    }
+}
+
+
+
+
+
+
 
 //https://www.cs.cmu.edu/~guna/15-123S11/Lectures/PPT/Lecture16.pdf
 
